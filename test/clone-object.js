4a7fa02ffae950db478ca54cf72460fc21fd4610
Natalia Antonova <5555482@gmail.com>
2015-10-28 20:53:37 +0000

    clone-object.js test

--- test/clone-object.js
+++ test/clone-object.js
@@ -2,8 +2,11 @@ describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
+        obj = JSON.parse(JSON.stringify(expected));
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
 });
+
+
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,23 @@
+
 describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+
+    var flatten = function(item, result) {
+      if (Object.prototype.toString.call(item) == '[object Array]') {
+        for (var i in item) {
+          flatten(item[i], result);
+        }
+      } else {
+        result.push(item);
+      }
+
+      return result;
+    };
+
+    arr = flatten(arr, []);
+    arr.sort();
 
     expect(arr).toEqual(expected);
   });
