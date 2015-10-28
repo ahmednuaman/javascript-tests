05fbc1a8a1656c7be5fa10b5f5a7571d4d3e501d
Natalia Antonova <5555482@gmail.com>
2015-10-28 21:13:45 +0000

    flattern-array

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,13 +2,11 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
     arr = arr.join().split(',').sort();
 
 		for(var i=0;i<arr.length;i++){
-			arr[i] = arr[i]/1;
+			return arr[i];
 		}
-
     expect(arr).toEqual(expected);
   });
 });
