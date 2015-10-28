df91d990dd060a86b2cf790a49b8068d6425f17c
Natalia Antonova <5555482@gmail.com>
2015-10-28 21:23:05 +0000

    3 tests passed

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,7 +3,6 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
     arr = arr.join().split(',').sort();
-
 		for(var i=0;i<arr.length;i++){
 			return arr[i];
 		}
