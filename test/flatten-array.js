264ef221eba62803b9a1683046428a66d11f000e
Natalia Antonova <5555482@gmail.com>
2015-10-28 21:02:55 +0000

    flatten-array

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,20 +3,11 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
-    var flatten = function(item, result) {
-      if (Object.prototype.toString.call(item) == '[object Array]') {
-        for (var i in item) {
-          flatten(item[i], result);
-        }
-      } else {
-        result.push(item);
-      }
+    arr = arr.join().split(',').sort();
 
-      return result;
-    };
-
-    arr = flatten(arr, []);
-    arr.sort();
+		for(var i=0;i<arr.length;i++){
+			arr[i] = arr[i]/1;
+		}
 
     expect(arr).toEqual(expected);
   });
