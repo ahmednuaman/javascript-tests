2465321bd556c642e1326da508f41aff7dd0f14e
Poul Cummings <poul@cummings.dk>
2017-03-05 21:07:29 +0000

    add test for flatten, clone and scoping

--- karma.conf.js
+++ karma.conf.js
@@ -21,20 +21,21 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      './node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js',
       'test/**/*.js'
     ],
 
 
     // list of files to exclude
     exclude: [
-      
+
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+
     },
 
 
