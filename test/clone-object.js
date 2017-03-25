7d96a64c881003539772ede7aff281fe12641861
wilson <willu2125@gmail.com>
2017-03-25 11:23:06 +0530

    Error Fixed!

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,20 @@
-describe('clone object', function () {
-  it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+function clone(obj) 
+{
+  return Object.assign({}, obj);
+}
+describe('clone object', function () 
+{
+  it('should clone an object', function () 
+  {
+    var expected = 
+    {
+		name: 'Ahmed', 
+		age: 27, 
+		skills: ['cycling', 'walking', 'eating']
+    },
+    
+    //obj = {};
+    obj = clone(expected);
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,17 @@
-describe('flatten array', function () {
-  it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
+function flatten(arr) {
+   return arr.reduce(function(acc, val) {
+     return acc.concat(Array.isArray(val) ? flatten(val) : val)
+   }, []).sort();
+ }
+
+describe('flatten array', function () 
+{
+	it('should flatten an array', function () 
+	{
+    	var arr = flatten([1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]]),
+        
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
-    expect(arr).toEqual(expected);
-  });
+    	expect(arr).toEqual(expected);
+  	});
 });
