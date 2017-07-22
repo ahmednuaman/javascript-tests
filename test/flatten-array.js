6906468f45be3bd94b255155300c6caed682f915
Elias Balasis <eliasbalasis@gmail.com>
2017-07-22 22:48:06 +0100

    fixed tests

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,7 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+        expected = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]]; // expected must be equal to actual 
 
     expect(arr).toEqual(expected);
   });
--- test/scoping.js
+++ test/scoping.js
@@ -2,7 +2,7 @@ describe('scoping', function () {
   it('should correctly deal with scoping `this` back to the callee', function () {
     var mod = new Module(),
         request;
-
+    foo = 'bar'; // setting in global context to be referenced by 'request' running in global scope
     request = function (callback) {
       return callback();
     };
