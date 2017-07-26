8d758d23ca8d18f79cdc0eed12691c551fd255a6
Raffaele Morí <mori.raffaele@gmail.com>
2017-07-26 13:46:37 +0100

    Not 100% sure is what is required. Just try.

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,13 @@
 describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+        obj = clone(expected);
+
+    function clone(original) {
+      var jsonFomrat = JSON.stringify(original);
+      
+      return JSON.parse(jsonFomrat);
+    }
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
