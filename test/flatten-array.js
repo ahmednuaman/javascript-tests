a86bb5472d905b36469aa2844e159d5ad206e4f4
Aleksandar Zivkovic <aleksandar.zivkovic@symphony.is>
2018-07-10 10:37:03 +0200

    tests fix

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,12 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    if (Array.isArray(arr)) {
+      const stringArray = JSON.stringify(arr);
+      const regex = new RegExp(/\[|\]/g);
+      arr = JSON.parse('[' + stringArray.replace(regex, '') + ']').sort();
+    }
+
     expect(arr).toEqual(expected);
   });
 });
