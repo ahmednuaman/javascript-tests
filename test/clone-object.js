d58d14f1b8a1ab0d80374bdb05baa9342b996f93
Stefan Ilijevski <stefan.ilijevski@symphony.is>
2018-07-10 10:04:55 +0200

    test fixes

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,7 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+    const expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
+        obj = JSON.parse(JSON.stringify(expected));
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
