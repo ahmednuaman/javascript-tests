86c5c0d854883dad849812d488d448787ab96fa4
Everistus Olumese <everistusolumese@gmail.com>
2018-09-21 13:11:09 +0100

    Solution

--- test/clone-object.js
+++ test/clone-object.js
@@ -3,7 +3,6 @@ describe('clone object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
         obj = Object.assign({}, expected);
-        
     expect(obj).toEqual(expected);
    expect(obj).not.toBe(expected);
   });
