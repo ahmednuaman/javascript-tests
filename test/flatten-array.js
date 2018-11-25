a7bf363ffcb4fa34866474bce4f1aad57e949c94
Toby Short <toby-short@outlook.com>
2018-11-25 18:45:17 +0000

    Tests completed

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,6 +1,7 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
+		arr = arr.concat(...arr);
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
     expect(arr).toEqual(expected);
