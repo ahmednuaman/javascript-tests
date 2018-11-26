74ae31d52fc9ea998fac1df53def48e8842de49e
Toby Short <toby-short@outlook.com>
2018-11-26 18:43:51 +0000

    fixed flatten array test

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,21 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-		arr = arr.concat(...arr);
+    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]];
+	
+		function flatten(tempArray) {
+			var flattenedArray = [];
+			for(var i = 0; i < tempArray.length; i++) {
+				if(Array.isArray(tempArray[i])) {
+					flattenedArray.push(...flatten(tempArray[i]));
+				}
+				else {
+					flattenedArray.push(tempArray[i]);
+				}
+			}
+			return flattenedArray.sort();
+		}
+
+		arr = flatten(arr);
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
     expect(arr).toEqual(expected);
