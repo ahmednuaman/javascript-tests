a2fffbfb0c978ef7c62ac92aea55bc949d251e31
zantonas <zantonas@gmail.com>
2018-12-11 18:54:10 +0000

    flatten-array.js now correctly flattens an array

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,15 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]];
+    var expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    function flatten(arr) {
+      return arr.reduce(function (flat, toFlatten) {
+        return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+      }, []);
+    }
+
+    arr = flatten(arr).sort();
     expect(arr).toEqual(expected);
   });
 });
