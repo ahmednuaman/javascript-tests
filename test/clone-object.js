04334348e51ebcedd7a600d2c91b1efe5228a849
James Knights <james.knights@tinderhouse.com>
2018-12-13 09:52:51 +0000

    Flatten Array

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,9 +1,82 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+
+    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']}, 
+    obj = new ObjectHelper().cloneObject(expected);
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
 });
+
+
+
+var ObjectHelper = /** @class */ (function () {
+ 
+
+  function ObjectHelper () {
+    this._createPolyfill();
+  }
+
+  ObjectHelper.prototype.cloneObject = function (src = null) {
+    let result = null;
+    try {
+      result = this._cloneObject(src);
+    } catch (e) {
+      result = this._cloneFromJSON(src);
+    }
+    return result;
+  }
+
+  ObjectHelper.prototype._cloneObject = function (src = null) {
+    let result = null;
+    if (src && typeof Object.assign === "function") {
+      result = Object.assign({}, src);
+    } else {
+      throw new TypeError ("Unexpected input or Object Assign not available");
+    }
+    return result;
+  }
+
+  ObjectHelper.prototype._cloneFromJSON = function (src = null) {
+    return (JSON.parse(JSON.stringify(src)));
+  }
+
+  ObjectHelper.prototype._createPolyfill = function (src = null) {
+    /** Polyfill from MDN for <= ES5 */
+    if (typeof Object.assign != 'function') {
+      // Must be writable: true, enumerable: false, configurable: true
+      Object.defineProperty(Object, "assign", {
+        value: function assign(target, varArgs) { // .length of function is 2
+          'use strict';
+          if (target == null) { // TypeError if undefined or null
+            throw new TypeError('Cannot convert undefined or null to object');
+          }
+    
+          var to = Object(target);
+    
+          for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
+    
+            if (nextSource != null) { // Skip over if undefined or null
+              for (var nextKey in nextSource) {
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                  to[nextKey] = nextSource[nextKey];
+                }
+              }
+            }
+          }
+          return to;
+        },
+        writable: true,
+        configurable: true
+      });
+    }
+  }
+
+  
+
+  return ObjectHelper
+
+})()
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,50 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
+    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]];
+    arr = new ArrayHelper().flattenArray(arr);
+    var expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
     expect(arr).toEqual(expected);
   });
 });
+
+
+
+
+var ArrayHelper = /** @class */ (function () {
+ 
+
+  function ArrayHelper () {}
+
+  
+  ArrayHelper.prototype.determineLevels = function (arr = null) {
+    let result = 1;
+    result = this.mapForInnerArrays(arr, result);
+    return result;
+  }
+  
+  ArrayHelper.prototype.mapForInnerArrays = function (arr = null, result = null) {
+    arr.map(el => {
+      if (Array.isArray(el)) {
+        result++;
+        result = this.mapForInnerArrays(el, result);
+      }
+    });
+    return result;
+  }
+
+  ArrayHelper.prototype.flattenArray = function (toFlatten = null) {
+    let result = [];
+    if (Array.isArray(toFlatten)) {
+      let levels = this.determineLevels(toFlatten);
+      toFlatten = toFlatten.flat(levels);
+      result = toFlatten.sort();
+    }
+    return result;
+  }
+  
+
+  return ArrayHelper;
+
+})()
+
+
