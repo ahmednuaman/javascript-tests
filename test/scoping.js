2758ff4a8d1ba6784e49a797614d206a5a7e2528
kingsley-ugwu <kingsley.ugwu@gmail.com>
2018-12-16 21:13:18 +0000

    Tests fixed

--- test/scoping.js
+++ test/scoping.js
@@ -16,7 +16,7 @@ describe('scoping', function () {
     };
 
     Module.prototype.req = function() {
-      return request(this.method);
+      return request(this.method.bind(this));
     };
 
     expect(mod.req()).toBe('bar');
