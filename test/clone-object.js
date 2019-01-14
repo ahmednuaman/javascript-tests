cc6d4460db1324e6fcaa75066f0a970b019a6c9e
deama <deama@mail.com>
2019-01-14 18:15:30 +0000

    Add files via upload

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,8 +1,18 @@
-describe('clone object', function () {
-  it('should clone an object', function () {
+describe('clone object', function () 
+{
+  it('should clone an object', function () 
+  {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
 
+	for( var field in expected )
+	{
+		if( expected.hasOwnProperty(field) )
+		{
+			obj[field] = expected[field];
+		}
+	}
+		
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,29 @@
-describe('flatten array', function () {
-  it('should flatten an array', function () {
+describe('flatten array', function () 
+{
+  it('should flatten an array', function () 
+  {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+		
+	recurse(arr, finish);
+	finish.sort();
+	arr = finish;
 
     expect(arr).toEqual(expected);
   });
 });
+
+function recurse(array, finish)
+{
+	for( var i = 0; i < array.length; i++ )
+	{
+		if( Number.isInteger(array[i]) )
+		{
+			finish.push(array[i]);
+		}
+		else
+		{
+			recurse(array[i], finish);
+		}
+	}
+}
