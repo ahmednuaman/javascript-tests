37cd410b018ed7aaea960c7356c8012d3db2d7eb
MandyArthur <Daviddcg8@kent.ac.uk>
2019-01-16 08:14:01 +0000

    Fixed array format error

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,4 +1,4 @@
-var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
+var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]];
 
 function flatten(arr1) {
 	return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flatten(val)) : acc.concat(val), []);
