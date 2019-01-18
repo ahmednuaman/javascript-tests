9a7afaf6ee26035a4bf3df9d9af30eac48d7c6ff
OcoCoco <a_missangu@hotmail.com>
2019-01-18 14:05:49 +0000

    Updated files

--- test/clone-object.js
+++ test/clone-object.js
@@ -4,11 +4,16 @@ describe('clone object', function () {
 	it('should clone an object', function () {
 		var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
 		obj = {};
+		// console.log(expected);
 
-		obj = Object.assign({}, expected);
-        //console.log(obj);
-        
-        expect(obj).toEqual(expected);
-        expect(obj).not.toBe(expected);
-    });
+		for (var attr in expected) {
+			if (expected.hasOwnProperty(attr)){
+				obj[attr] = expected[attr];
+			}
+		}
+
+	    // console.log(obj);
+	    expect(obj).toEqual(expected);
+	    expect(obj).not.toBe(expected);
+	});
 });
