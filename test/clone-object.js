55533e980625f9c6d40e8c12055314303968ebb3
OcoCoco <a_missangu@hotmail.com>
2019-01-18 12:48:35 +0000

    JavaScript Tests

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,9 +1,14 @@
-describe('clone object', function () {
-  it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+//clone-object.js
 
-    expect(obj).toEqual(expected);
-    expect(obj).not.toBe(expected);
-  });
+describe('clone object', function () {
+	it('should clone an object', function () {
+		var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
+		obj = {};
+
+		obj = Object.assign({}, expected);
+        //console.log(obj);
+        
+        expect(obj).toEqual(expected);
+        expect(obj).not.toBe(expected);
+    });
 });
--- test/scoping.js
+++ test/scoping.js
@@ -1,10 +1,12 @@
+//scoping.js
+
 describe('scoping', function () {
   it('should correctly deal with scoping `this` back to the callee', function () {
     var mod = new Module(),
         request;
 
     request = function (callback) {
-      return callback();
+      return callback;
     };
 
     function Module () {
@@ -16,7 +18,7 @@ describe('scoping', function () {
     };
 
     Module.prototype.req = function() {
-      return request(this.method);
+      return request(this.method());
     };
 
     expect(mod.req()).toBe('bar');
