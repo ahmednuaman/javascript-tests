9ce06c8f5e2dea79c5d5843d1b5d72e853f70fc2
Roman Burdun <rburdun@gmail.com>
2019-01-23 15:29:20 +0000

    Fix: Changed implementation of test 3

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,10 +1,14 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
+
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
-    let arr_flat = arr.flat(4);
-    arr = arr_flat.sort();
+        let temp_arr_str = arr.toString();
+        const splittedArr = temp_arr_str.split(",");
+        arr = splittedArr.sort();
+        arr = arr.map(Number);
+
     expect(arr).toEqual(expected);
   });
 });
