a5507504a078d352c61e880851016f9157fd943d
Roman Burdun <rburdun@gmail.com>
2019-01-23 14:42:44 +0000

    Fix: All tests fixed

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,8 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    let arr_flat = arr.flat(4);
+    arr = arr_flat.sort();
     expect(arr).toEqual(expected);
   });
 });
