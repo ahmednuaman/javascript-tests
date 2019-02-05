648b082b29d8809a4c1e29525d268bc444d66e14
Lucian <paunalucian14@gmail.com>
2019-02-05 16:56:32 +0000

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,12 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+    
+    expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+    
+    var tempArrStr = arr.toString();
+    var splittedArr = tempArrStr.split(',');
+    arr = splittedArr.sort().map(Number);
 
     expect(arr).toEqual(expected);
   });
