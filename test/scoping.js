2655b964cecc737567505c4ed08d3609cd2fb914
Darryl Banks <darryl.banks@zombie9.com>
2019-02-12 14:42:50 +0000

    refactor bind out of scoping.js for phantomjs

--- test/scoping.js
+++ test/scoping.js
@@ -16,7 +16,7 @@ describe('scoping', function () {
     };
 
     Module.prototype.req = function() {
-      return request(this.method.bind(this));
+      return request(this.method()));
     };
 
     expect(mod.req()).toBe('bar');
