61e7140811953e9fa83d746c76a6aa7e8568ec20
Darryl Banks <darryl.banks@zombie9.com>
2019-02-12 14:27:11 +0000

    refactor flatten-array so phantomjs passes

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,7 +2,12 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-    arr = arr.flat(4).sort();
+    function flatten(myArray) {
+      return myArray.reduce(function (flat, toFlatten) {
+        return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+      }, []);
+    }
+    arr = flatten(arr).sort();
     expect(arr).toEqual(expected);
   });
 });
