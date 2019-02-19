a378e0721cad45753498308db7ec3cb0d2be06ef
Toni-Leigh Sharpe <toni.leigh@sage.com>
2019-02-19 21:45:25 +0000

    Used lodash to solve two problems
    * I'll always reach for a library like lodash to solve these problems rather than rolling my own solution.
    * Constantly re-writing functions like this each time they're needed leads to maintenance overhead.
    * I'm unlikely to do as good a good as an excellent library like Lodash.

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,12 @@
+var cloneDeep = require('lodash/cloneDeep');
+
 describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
+
+    // spread syntax could be used in modern ES, obj = { ...expected }
+    obj = cloneDeep(expected);
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
