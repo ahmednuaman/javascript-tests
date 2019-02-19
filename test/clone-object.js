de2e587a1711a6ad4b3274193c333acf3ce97ca3
Marinelli1980 <kkings@wp.eu>
2019-02-19 18:18:48 +0000

    flatten array with comments - done

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,11 @@
-describe('clone object', function () {
-  it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+describe("clone object", function() {
+  it("should clone an object", function() {
+    var expected = {
+        name: "Ahmed",
+        age: 27,
+        skills: ["cycling", "walking", "eating"]
+      },
+      obj = {};
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,19 @@
-describe('flatten array', function () {
-  it('should flatten an array', function () {
+describe("flatten array", function() {
+  it("should flatten an array", function() {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+      expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+
+    //on beggining I use spread operator to copy arr
+    //as returned array must be a new array (array must not mutated)
+    //in that copied array I use flat method to flatten array,
+    //inside bracket, number is deep a nested array structure should be flattened.
+    //then I use method sort to sort that array,
+    //I don't have to use "function(a, b){return b - a}" as we have numbers from 1 to 9,
+    //if we will have numbers 10 and over or -10 and less then we will have to
+    //sorting method will take only first character from number and then sorting
+    //and on the end assign NEW created array to arr variable
+
+    arr = [...arr].flat(3).sort();
 
     expect(arr).toEqual(expected);
   });
