bd953d3dfbae2706c5df296d1fd41c8c45c370ca
mctrl <mctrl@users.noreply.github.com>
2019-03-11 23:35:16 +0000

    pass code for tests on files

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,10 @@
+
+
 describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+        obj = Object.assign({}, expected); /* to copy inherited properties as well */
+        /*or obj = {...expected}  for shallow copy of object */
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,19 @@
+function flatten_array(arr, indent) {
+  for (let index = 0; index < indent; index++) {
+      arr = arr.reduce(function(a, b) {
+          return a.concat(b);
+      }, [])
+  }
+  return arr.sort();
+}
+
 describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
-    expect(arr).toEqual(expected);
+    var flat = flatten_array(arr, 3);
+
+    expect(flat).toEqual(expected);
   });
 });
