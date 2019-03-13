f495f65b2ed47ed77749d87ea11bd76bfb7487bc
Claret15 <37543659+Claret15@users.noreply.github.com>
2019-03-13 20:24:11 +0000

    Format code correctly

--- test/clone-object.js
+++ test/clone-object.js
@@ -3,7 +3,7 @@ describe('clone object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
 
-        obj={...expected};
+    obj = { ...expected };
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,9 +3,9 @@ describe("flatten array", function() {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
       expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
-      let flattenSortArray = array => array.reduce(
-        (a, b) => Array.isArray(b) ? a.concat(flattenSortArray(b)) : a.concat(b), []
-      ).sort((a, b) => a - b);
+    let flattenSortArray = array => array.reduce(
+      (a, b) => Array.isArray(b) ? a.concat(flattenSortArray(b)) : a.concat(b), []
+    ).sort((a, b) => a - b);
 
     expect(flattenSortArray(arr)).toEqual(expected);
   });
