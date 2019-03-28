dab5ae9f64746b896f8cf2f98596feba92917bed
TLRidgway <48688512+TLRidgway@users.noreply.github.com>
2019-03-28 16:10:44 +0000

    Add files via upload

---
+++ scoping.js
@@ -0,0 +1,26 @@
+describe('scoping', function () {
+  it('should correctly deal with scoping `this` back to the callee', function () {
+    var mod = new Module(),
+        request;
+
+    request = function (callback) {
+      return callback();
+    };
+
+    function Module () {
+      this.foo = 'bar';
+    }
+
+    Module.prototype.method = function() {
+      return foo;
+    };
+
+    Module.prototype.req = function() {
+      return request(this.method);
+    };
+
+    expect(mod.req()).toBe('bar');
+	
+	//no idea how to attempt this one
+  });
+});
