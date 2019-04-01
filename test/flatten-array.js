af37b187aafc4b101ace0f498bd886cf0c612e24
Aaron Pedwell <ap712@kent.ac.uk>
2019-04-01 18:44:25 +0100

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,9 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    const myArray2 = arr.flat(Infinity);
+    arr = myArray2.sort();
+
     expect(arr).toEqual(expected);
   });
 });
