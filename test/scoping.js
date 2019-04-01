e81044835967fac21168e71d4ec0c792a9df4bc8
NathanB09 <nathanbell09@gmail.com>
2019-04-01 13:01:19 +0100

    scoping test working

--- test/scoping.js
+++ test/scoping.js
@@ -1,22 +1,22 @@
 describe('scoping', function () {
   it('should correctly deal with scoping `this` back to the callee', function () {
     var mod = new Module(),
-        request;
+      request;
 
     request = function (callback) {
       return callback();
     };
 
-    function Module () {
+    function Module() {
       this.foo = 'bar';
     }
 
-    Module.prototype.method = function() {
+    Module.prototype.method = function () {
       return this.foo;
     };
 
-    Module.prototype.req = function() {
-      return request(this.method);
+    Module.prototype.req = function () {
+      return request(this.method.bind(this));
     };
 
     expect(mod.req()).toBe('bar');
