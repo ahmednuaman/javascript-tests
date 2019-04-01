e41276ab2602cea470fa00c9b5724b5b4e96508d
NathanB09 <nathanbell09@gmail.com>
2019-04-01 11:48:59 +0100

    clone-object test complete

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,9 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+    var expected = { name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating'] },
+      obj = {};
+
+    obj = { ...expected }
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
