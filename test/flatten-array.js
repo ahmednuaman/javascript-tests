d87510d0022fab82d57d3aec8f89c192597ce979
Samuel Catley <scp94373@gmail.com>
2019-04-03 14:52:10 +0100

    House Clean
    Tidying up variables used.

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,14 +1,12 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]];
-    // var arr2 = arr.flat(Infinity);
-    // arr = arr2.sort();
     expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-        arr = arr.toString();
-        arr = arr.split(',');
-        arr = arr.sort();
-        arr = arr.map(Number);
+        strArr = arr.toString();
+        splArr = strArr.split(',');
+        sorArr = splArr.sort();
+        comArr = sorArr.map(Number);
 
-    expect(arr).toEqual(expected);
+    expect(comArr).toEqual(expected);
   });
 });
