912711642bd6fa1005680ac9da60096b54eb20a7
Samuel Catley <scp94373@gmail.com>
2019-04-03 13:51:37 +0100

    All tests passing
    Completed Clone-Object, Flatten-Array and Scoping tests.

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,13 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]];
+    // var arr2 = arr.flat(Infinity);
+    // arr = arr2.sort();
+    expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+        arr = arr.toString();
+        arr = arr.split(',');
+        arr = arr.sort();
+        arr = arr.map(Number);
 
     expect(arr).toEqual(expected);
   });
