1e78d4b68ac66502027097f778926e764d3596bc
ajpwilson <wilajp@gmail.com>
2019-04-04 18:35:23 +0100

    Andy-Wilson-Test

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,8 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']};
+
+    const obj = {...expected};
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
