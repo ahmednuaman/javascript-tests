e9162bb4d1b831c838a8094bb1d372f44739eac5
jfarre90 <47380600+jfarre90@users.noreply.github.com>
2019-04-11 10:52:50 +0100

    Solution to clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,9 +1,17 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
+    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']};
         obj = {};
-
+        
+        //SOLUTION - Deep copy
+        let cloneObject = obj => JSON.parse(JSON.stringify(obj));
+    
+        
     expect(obj).toEqual(expected);
-    expect(obj).not.toBe(expected);
+    expect(obj).not.toBe(expected);  
+    
+    expect(cloneObject(expected)).toEqual(expected);
+    expect(cloneObject(expected)).not.toBe(expected);
+    
   });
 });
