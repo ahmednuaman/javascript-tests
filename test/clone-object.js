0f49b41dbc7ed0cfc1803802aa0e4cc0561cba25
Javier Farre <jfarre90@gmail.com>
2019-04-11 10:43:45 +0000

    Modified dependencies due to issues being experienced on the CI build

--- test/clone-object.js
+++ test/clone-object.js
@@ -2,8 +2,16 @@ describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
-
+        
+      //SOLUTION - Deep copy
+      let cloneObject = obj => JSON.parse(JSON.stringify(obj));
+    
+        
     expect(obj).toEqual(expected);
-    expect(obj).not.toBe(expected);
+    expect(obj).not.toBe(expected);  
+    
+    expect(cloneObject(expected)).toEqual(expected);
+    expect(cloneObject(expected)).not.toBe(expected);
+    
   });
 });
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,7 +2,19 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
+    
+    //SOLUTION
+    let flatAndSortArray = arr => {
+      
+      //Finding the depth of the array
+      let arrayDepth= arr=>Array.isArray(arr)? Math.max(...arr.map(arrayDepth))+1:0;
+      
+      //Flattening and sorting
+      return arr.flat(arrayDepth(arr))
+       .sort((a, b) => a-b);
+    };
+    
     expect(arr).toEqual(expected);
+    expect(flatAndSortArray(arr).toEqual(expected));
   });
 });
