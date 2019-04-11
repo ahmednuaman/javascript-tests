e1be46079cd44583762d8a71ecae4ab8c849094a
Ashley Pereira <38704306+ajpereira02@users.noreply.github.com>
2019-04-11 11:47:19 +0100

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,7 +2,11 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
+    let flattenArray = array => array
+      .reduce((num1, num2) => Array.isArray(num2) ? num1
+       .concat(flattenArray(num2)) : num1.concat(num2), [])
+       .sort((num1, num2) => a - b);
+              
     expect(arr).toEqual(expected);
   });
 });
