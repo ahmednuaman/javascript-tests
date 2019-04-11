59f2653ec5c877820b18e4bfc2ea4029085f58e9
Javier Farre <jfarre90@gmail.com>
2019-04-11 11:31:17 +0000

    Changed flatten-array solution to a more efficient one

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -4,16 +4,9 @@ describe('flatten array', function () {
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
     
     //SOLUTION
-    let flatAndSortArray = arr => {
-      
-      //Finding the depth of the array
-      let arrayDepth = arr=>Array.isArray(arr)? Math.max(...arr.map(arrayDepth))+1:0;
-      
-      //Flattening and sorting
-      return arr.flat(arrayDepth(arr))
-       .sort((a, b) => a-b);
-    };
+    arr = arr.toString().split(',').sort().map(Number);
     
-    expect(flatAndSortArray(arr)).toEqual(expected);
+    
+    expect(arr).toEqual(expected);
   });
 });
