e300f0d83a2c4d99f0fabc9a74bb95bd74bd0a3f
aaroncherry <49523110+aaroncherry@users.noreply.github.com>
2019-04-11 13:52:50 +0100

    Update and rename clone-object.js to aaronCherry.js

--- test/clone-object.js
+++
@@ -1,9 +0,0 @@
-describe('clone object', function () {
-  it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
-
-    expect(obj).toEqual(expected);
-    expect(obj).not.toBe(expected);
-  });
-});
