0e72a56be2a9c6d0d49ef419a76f37127f042bba
Malvin G Harding <hardingm@aston.ac.uk>
2019-04-12 18:34:26 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.js'
+      "src/**/*.js",
+      "test/**/*.js"
     ],
 
 
@@ -34,7 +35,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+
     },
 
 
