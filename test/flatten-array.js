8ab2457fc6f2874021f30e4332999e233d076963
Malvin G Harding <Malvinharding@hotmail.co.uk>
2019-04-12 17:13:50 +0100

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,10 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
+    const arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
-    expect(arr).toEqual(expected);
+        
+	  	const newArr = arr.flat(Infinity).sort();
+	  
+    expect(newArr).toEqual(expected);
   });
 });
