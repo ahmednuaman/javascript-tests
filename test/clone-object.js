8ec6798af2308492f895228ca3e4220e48c89ebd
Malvin G Harding <Malvinharding@hotmail.co.uk>
2019-04-12 17:04:20 +0100

    Update clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,9 +1,10 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+    const expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
 
-    expect(obj).toEqual(expected);
-    expect(obj).not.toBe(expected);
+        const clone = JSON.parse(JSON.stringify(expected));
+
+    expect(clone).toEqual(expected);
+    expect(clone).not.toBe(expected);
   });
 });
