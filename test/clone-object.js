68b450e319f122ba4e077ec72abbcfcb768024d3
Rafa <rrl89@hotmail.com>
2019-04-17 15:23:33 +0200

    updating clone-object.js to fix it

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,24 +1,11 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
+    let expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']};
     let obj = {};
 
-    //in this case I iterate all the properties of the original object and 
-    //during the loop I am assigning each one to the new created object
-    
-     function iterationCopy(expected) {
-	  let target = {};
-		  for (let prop in src) {
-		    if (src.hasOwnProperty(prop)) {
-		      target[prop] = src[prop];
-		    }
-		  }
-	  obj = target;
-	 }
+    obj = JSON.parse(JSON.stringify(expected));
 
 	 // Other solutions that are also working:
-
-	 //obj = JSON.parse(JSON.stringify(expected));
 
     // obj2 = { ...expected };
 
