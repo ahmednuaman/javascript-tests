669acba8a7eb17d051a36d52ab32c6b023e88e18
Yolanda Collins <landa.collins14@gmail.com>
2019-04-18 13:27:49 +0100

    All test complete and successful

--- test/clone-object.js
+++ test/clone-object.js
@@ -3,6 +3,9 @@ describe('clone object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
 
+    /* Utilised JSON method, however if expected object had inner objects or functions i would of used a for...in to iterate over the object to copy it. */
+    obj = JSON.parse(JSON.stringify(expected));
+
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,17 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    // Implemented this as a function so that the ternary operator could recursively call it. 
+    function flattenArr(myArray) {
+      return myArray.reduce(function (flat, next) {
+        return flat.concat(Array.isArray(next) ? flattenArr(next) : next);
+      }, []);
+    }
+    
+    // With the current array a simple sort implemented on the flattened array is enough.
+    arr = flattenArr(arr).sort();
+
     expect(arr).toEqual(expected);
+
   });
 });
