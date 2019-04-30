f1fa44f7af5387a309abe651b540384cd362afcb
JuanMateoEhrenberg <50135082+JuanMateoEhrenberg@users.noreply.github.com>
2019-04-30 12:57:32 +0100

    Update scoping.js

--- test/scoping.js
+++ test/scoping.js
@@ -1,22 +1,22 @@
 describe('scoping', function () {
   it('should correctly deal with scoping `this` back to the callee', function () {
-    var mod = new Module(),
+     var mod = new Module(),
         request;
 
     request = function (callback) {
-      return callback();
+      return (callback);
     };
 
-    function Module () {
-      this.foo = 'bar';
+    function Module() {
+     this.foo ='bar';
     }
 
     Module.prototype.method = function() {
-      return this.foo;
+      this.foo = 'bar';
     };
 
     Module.prototype.req = function() {
-      return request(this.method);
+      return request(this.foo);
     };
 
     expect(mod.req()).toBe('bar');
