fbaabcfe442e453cb4b9366366984c5a16bf6423
unknown <robfjohnson@hotmail.com>
2019-05-19 10:52:00 +0100

    Rob Johnson, Development Manager, Interview

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,42 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    // init vars
+    var output = [];
+    var count = 0;
+
+    // build recursive function
+    recursiveFunc = function (recArr) {
+      if (Array.isArray(recArr)) {
+        // if parameter is an array then iterate each position
+        for (z=0; z<recArr.length; z++) {
+          var nextArr = recArr[z];
+          // if it has more than 1 entry then recurvisve call
+          if (nextArr.length > 1) {
+            recursiveFunc(nextArr);
+          } else {
+            // if it is an array of only 1 then no need to call recursive just add to output
+            output[count]=nextArr;
+            count++;
+          }
+        }
+      } else {
+        // not an array just add to output
+        output[count]=recArr;
+        count++;
+      }
+    };
+
+    // iterate top level array all the way through
+    for (i=0; i<arr.length; i++) {
+      // create constant from next value
+      const newarr = arr[i];
+      // start processing
+      recursiveFunc(newarr);
+    }
+
+    arr = output.sort();
+    
     expect(arr).toEqual(expected);
   });
 });
