ef669162fa7c758e3a785a2b9144bc44776df857
Sergej-Vlasov <sergej.s.vlasov@gmail.com>
2019-05-19 22:43:49 +0100

    solved flatten-array

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,9 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
-    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+    var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]];
+    var expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+
+    arr = arr.toString().split(',').sort().map(Number);
 
     expect(arr).toEqual(expected);
   });
