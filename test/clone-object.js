8ef933daa380ef57d432ed1be0e82bbb06fa28cb
BudacaB <budaca.b@gmail.com>
2019-05-21 23:00:44 +0300

    Solved Tests

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,9 +1,15 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
 
+    // Arrange
+    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
+    obj = {};
+
+    // Act
+    obj = Object.assign(obj, expected);
+
+    // Assert
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
-  });
+  }); 
 });
