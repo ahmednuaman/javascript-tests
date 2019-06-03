affacee08817a2836fa6a8da20e726e1fbf0ee6f
muradalam <42180716+muradalam@users.noreply.github.com>
2019-06-03 15:56:13 +0100

    Update clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,23 +1,11 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
+var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
 
-    //Get all keys from object
-    var keyarrs = Object.keys(expected);
+    obj = JSON.parse(JSON.stringify(expected));
 
-    //iterate keys and assign with value to blank object
-    for (x=0; x<keyarrs.length; x++) {
-        obj[keyarrs[x]] = expected[keyarrs[x]];
-    }
-
-    //check equality
-    // console.log(JSON.parse(JSON.stringify(obj)));
-    // console.log(JSON.parse(JSON.stringify(expected)));
-
-    // obj is equal to expected
     expect(obj).toEqual(expected);
-    // object is not expected
     expect(obj).not.toBe(expected);
   });
 });
