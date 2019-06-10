cd288bdff230f23939edaf0e4b766f1268b51c27
Mahmoud Khalifa <mahmoudkhalifa@hotmail.co.uk>
2019-06-10 22:32:12 +0100

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,6 +2,23 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+    
+    function flatten(arr) {
+          var flatten = [];
+          !(function flat(array) {
+          array.forEach(function(el) {
+            if (Array.isArray)(el)) flat(el);
+            else flattend.push(el);
+          });  
+          }) (array);
+          return flattend;
+        }
+
+       
+
+        var flattenAndSorted = flatten(arr).sort();
+
+        console.log(flattenAndSorted)
 
     expect(arr).toEqual(expected);
   });
