2fe7a44836bfce41eab93deca9be6f3e2141ff92
Wesley Twyman <wesley@twyman.io>
2019-06-10 11:46:37 +0100

    Completed.

--- test/clone-object.js
+++ test/clone-object.js
@@ -3,6 +3,13 @@ describe('clone object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
 
+    // This is not supported in Internet Explorer, if Internet Explorer is required, then use the solution below.
+    // This is also not suitable for deep cloning of an object.
+    obj = Object.assign({}, expected);
+
+    // This isn't as nice, but supports Internet Explorer and deep cloning:
+    // obj = JSON.parse(JSON.stringify(expected));
+
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,16 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    // This isn't supported in Internet Explorer or Edge.
+    // arr = arr.flat(5);
+
+    // This is supported in everything, but not as nice.
+    function flattenDeep(arr) {
+      return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+    }
+
+    arr = flattenDeep(arr);
+
     expect(arr).toEqual(expected);
   });
 });
