2ed5286cf538f58a26af6fff7420a78c63ec0d9f
Banjer71 <davgio7@gmail.com>
2019-06-11 10:05:21 +0100

    test solved

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,7 +2,12 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
+      return Array.isArray(arr) ?
+        arr.concat(flatArr(arr)) : arr.concat(arr, []);
     expect(arr).toEqual(expected);
   });
 });
+
+function flatArr(arr) {
+  return arr = [].concat(...arr);
+}
