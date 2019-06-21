3fb6a79b197d08b127511e69f062206a9309db89
OllieMartin <olivermartin410@gmail.com>
2019-06-21 15:33:09 +0100

    Fixed the test scripts

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,11 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    const arrAsString = arr.toString();
+    const flatArr = arrAsString.split(',').map(Number);
+    
+    arr = flatArr.sort();
+
     expect(arr).toEqual(expected);
   });
 });
