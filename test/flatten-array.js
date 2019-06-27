b3defc25e48b1e0a34a478febbae99986bf8a94e
LetMeRoll <52294123+LetMeRoll@users.noreply.github.com>
2019-06-28 00:29:05 +0100

    fixed test and update dependencies
    update karma dependencies
    update phantomjs dependencies
    fix issues with test scripts

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,11 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    const arrString = arr.toString(),
+      arrSplit = arrString.split(',');
+    
+    arr = arrSplit.map(Number).sort();
+
     expect(arr).toEqual(expected);
   });
 });
