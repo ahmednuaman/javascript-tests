192b3f812954e0287a96ea210864980bb625e7a5
Peter Styk <peter@styk.tv>
2019-07-15 13:56:47 +0100

    go back to dual browser check

--- karma.conf.js
+++ karma.conf.js
@@ -55,8 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: browsers,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
