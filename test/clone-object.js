189debda0f58a735f6af102428b72befe23615ef
seke626 <52959360+seke626@users.noreply.github.com>
2019-07-16 15:33:57 +0100

    Solution to clone test

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,17 @@
+/**
+ * This function converts to Json and copies from there
+ */
+ 
+
+function jsonCopy(src) {
+  return JSON.parse(JSON.stringify(src));
+}
+
+
 describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+        obj = jsonCopy(expected);
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
