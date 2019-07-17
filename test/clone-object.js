4b76c703cec9db5c086a76d4c1719eea92bc7e45
racheljinks <53005222+racheljinks@users.noreply.github.com>
2019-07-17 15:07:49 +0100

    Update clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -3,7 +3,7 @@ describe('clone object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
     
-    obj = JSON.parse{JSON.stringify(expected)}
+    obj = JSON.parse(JSON.stringify(expected))
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
