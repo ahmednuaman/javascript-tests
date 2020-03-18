8a105dea3deb6e5d09cd74de91885e38e94fceb0
BenCollins97 <42169342+BenCollins97@users.noreply.github.com>
2020-03-18 14:52:30 +0000

    Update clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,7 @@
 describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-//         efficient one line to clone objects
+//         efficient one line to clone objects.
         obj = JSON.parse(JSON.stringify(expected));
 
     expect(obj).toEqual(expected);
