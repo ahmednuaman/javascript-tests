077709a863056692f4522efe9fceffb723ae877b
BenCollins97 <42169342+BenCollins97@users.noreply.github.com>
2020-03-18 14:26:24 +0000

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,7 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
     
+//     Using inbuilt flatten and sort functionality in a neat one liner
     arr = arr.flat(Infinity).sort(function(a, b){return a-b});
 
     expect(arr).toEqual(expected);
