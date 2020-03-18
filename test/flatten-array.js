9592d2690f5bd12c414fb9188b56f778071b23a0
BenCollins97 <42169342+BenCollins97@users.noreply.github.com>
2020-03-18 13:55:22 +0000

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,6 +2,8 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+    
+    arr = arr.flat(Infinity).sort(function(a, b){return a-b});
 
     expect(arr).toEqual(expected);
   });
