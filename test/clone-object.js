a45ca6340de3a4c67caa276e852cea4144c6f39e
ziggy-98 <luke@reallyquite.com>
2020-07-16 11:22:54 +0100

    solutions to tests

--- test/clone-object.js
+++ test/clone-object.js
@@ -3,6 +3,10 @@ describe('clone object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
 
+        Object.keys(expected).forEach(function(key, index){
+          obj[key] = expected[key];
+        });
+
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
   });
