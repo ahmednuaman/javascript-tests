9001607a10e55faeb47e9a9af87bb1f1a87dc566
RebeccaHough <rebecca.c.hough@blueyonder.co.uk>
2020-07-20 15:36:29 +0100

    add solution

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,12 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    function flatten (arr) {
+      return arr.reduce((acc, curr) => Array.isArray(curr) ? acc.concat(flatten(curr)): acc.concat(curr), [])
+    }
+    
+    arr = flatten(arr).sort()
+
     expect(arr).toEqual(expected);
   });
 });
