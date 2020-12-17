5d1711f22336d00c791bd5386f3b6ca725801f4d
acunil <locamos@gmail.com>
2020-12-17 11:38:00 +0000

    formatting

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,7 +1,7 @@
-describe("flatten array", function () {
-  it("should flatten an array", function () {
+describe('flatten array', function () {
+  it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
-      expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
+        expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
     // arr.flat() does not work on IE
 
--- test/scoping.js
+++ test/scoping.js
@@ -1,24 +1,24 @@
-describe("scoping", function () {
-  it("should correctly deal with scoping `this` back to the callee", function () {
+describe('scoping', function () {
+  it('should correctly deal with scoping `this` back to the callee', function () {
     var mod = new Module(),
-      request;
+        request;
 
     request = function (callback) {
       return callback();
     };
 
-    function Module() {
-      this.foo = "bar";
+    function Module () {
+      this.foo = 'bar';
     }
 
-    Module.prototype.method = function () {
+    Module.prototype.method = function() {
       return this.foo;
     };
 
-    Module.prototype.req = function () {
+    Module.prototype.req = function() {
       return request(this.method.bind(this));
     };
 
-    expect(mod.req()).toBe("bar");
+    expect(mod.req()).toBe('bar');
   });
 });
