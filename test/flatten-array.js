23d449b31e573bb92c3c60dd9bcbd79c87e37e9a
Basilmeister <81642596+Basilmeister@users.noreply.github.com>
2021-03-30 18:37:51 +0100

    Update flatten-array.js

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -1,8 +1,9 @@
 describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
+    var flatArr = arr.flat(Infinity);
+    arr = flatArr.sort();
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
     expect(arr).toEqual(expected);
   });
 });
