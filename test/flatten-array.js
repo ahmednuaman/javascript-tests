7f6e5321e3271f60206e168c60dc3f939e74bf1b
TJCodes <t_kittoe@hotmail.co.uk>
2021-05-06 13:26:48 +0100

    Tests working

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,23 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+        function flattenArray(a) {
+          var flattened = [];
+
+          a.forEach(function(element) {
+            if(Array.isArray(element)) {
+              flattened = flattened.concat(flattenArray(element))
+            }
+            else if(typeof(element) === 'number') {
+              flattened.push(element);
+            }
+          });
+
+          return flattened.sort();
+        }
+
+        arr = flattenArray(arr);
+
     expect(arr).toEqual(expected);
   });
 });
