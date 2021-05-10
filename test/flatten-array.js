ef0854c6c2ee0993a9f66d46bd1b24414d730dc8
Hasan Ali <hasanhaja@gmail.com>
2021-05-10 18:19:21 +0400

    Update config files and make the tests pass.

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,7 +2,7 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
+    arr = arr.flat(3).sort();
     expect(arr).toEqual(expected);
   });
 });
