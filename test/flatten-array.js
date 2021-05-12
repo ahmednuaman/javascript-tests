d8cd58ffa303dfad8694c77b5a349ff647b26c15
Vicente Manzano <46681871+DevVincent@users.noreply.github.com>
2021-05-12 07:25:33 +0100

    All three tests are sucessfull

--- test/flatten-array.js
+++ test/flatten-array.js
@@ -2,7 +2,21 @@ describe('flatten array', function () {
   it('should flatten an array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
-
+        
+        const flattenArray = (arr) => {
+          someNewArray = arr.reduce((acc, item)=>{
+              if(Array.isArray(item)){
+                  acc = acc.concat(flattenArray(item))
+              }
+              else{
+                  acc = [...acc, item]
+              }
+              return acc
+          },[])
+          return someNewArray.sort()
+      }
+      
+    arr = flattenArray(arr)
     expect(arr).toEqual(expected);
   });
 });
