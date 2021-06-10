17c2f6e0b6825996f2d52cff691fce084614b3d9
bardenha <bardenha@gmail.com>
2021-06-10 14:17:35 +0100

    All solutions implemented and tests passed

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,7 +1,14 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
-        obj = {};
+    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},    
+  
+        /* Shallow copy methods:
+        obj = {...expected};
+        obj = Object.assign({}, expected);
+        */
+
+        // Deep clone method without using external libraries
+        obj = JSON.parse(JSON.stringify(expected));
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
--- test/flatten-array.js
+++ test/flatten-array.js
@@ -3,6 +3,12 @@ describe('flatten array', function () {
     var arr = [1, 2, [1, 2, [3, 4, 5, [1]]], 2, [2]],
         expected = [1, 1, 1, 2, 2, 2, 2, 3, 4, 5];
 
+    // Flattens array of infinite depth
+    arr = arr.flat(Infinity);
+
+    // Sorts array to give expected output
+    arr = arr.sort();
+
     expect(arr).toEqual(expected);
   });
 });
