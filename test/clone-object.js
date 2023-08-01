ac3633e64aed59ae99cdc4cdce1abf0c4258abd4
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-08-01 19:53:15 +0530

    Update clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -1,6 +1,6 @@
 describe('clone object', function () {
   it('should clone an object', function () {
-    var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
+    let expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
 
     expect(obj).toEqual(expected);
